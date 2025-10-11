698d1a7223a4f53f36a2d0f82be5a65b0416c881
Dhairya <dhairyadave962@gmail.com>
2025-10-11 11:48:13 +0530

    Custom API response and error handeling in utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import { connectDB } from "./db/db.js"
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.error("Database connection failed in index file", err);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+// A way of wirting async handler using Promises
+const asyncHandler = (requestHandlerFunction) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandlerFunction(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// A breadkdown for HOF wirting async handler
+/*
+    const asyncHandler = (fn) => {} The general structure of HOF
+    const asyncHandler = (fn) => {() => {}} The ffunction returns another function
+    const asyncHandler = (fn) => async () => {} The returned function same is above with
+                                                async keyword and imporoved syntax
+*/
+
+// A way of wirting async handler using try catch block
+/* 
+    const asyncHandler = (fn) => async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            res.status(err.code || 500).json({
+                success: false,
+                message: err.message || "Internal Server Error"
+            })
+        }    
+    }
+*/
