63afd9de339a594bb9d696ca9dc9b6360fff8010
Dhairya <dhairyadave962@gmail.com>
2025-10-30 11:19:05 +0530

    Basic setup of user controller and routes

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// The standard production practice is as follows import the routes here and then declare
+import userRouter from "./routes/user.routes.js";
+
+// Routes declaration
+app.use("/api/v1/users", userRouter)
+// Generally in production ruotes have proper defination like is it an api or the version etc
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -12,7 +12,7 @@ export { asyncHandler };
 // A breadkdown for HOF wirting async handler
 /*
     const asyncHandler = (fn) => {} The general structure of HOF
-    const asyncHandler = (fn) => {() => {}} The ffunction returns another function
+    const asyncHandler = (fn) => {() => {}} The function returns another function
     const asyncHandler = (fn) => async () => {} The returned function same is above with
                                                 async keyword and imporoved syntax
 */
