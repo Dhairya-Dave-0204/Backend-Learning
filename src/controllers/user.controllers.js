dee9a8701270383a4e8a98d4cba0835e5b9e2b06
Dhairya <dhairyadave962@gmail.com>
2025-12-13 11:47:22 +0530

    Create getWatchHistory endpoint

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -6,6 +6,7 @@ import {
   deleteFromCloudinary,
 } from "../utils/cluodinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -441,6 +442,60 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch hsitory fetched successfully"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -452,4 +507,5 @@ export {
   updateUserAvatar,
   updateUserCover,
   getUserChannelProfile,
+  getWatchHistory,
 };
