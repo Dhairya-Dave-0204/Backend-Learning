a4b69fd1a94175cf0eb87b657f0fbace63886121
Dhairya <dhairyadave962@gmail.com>
2026-01-14 19:49:40 +0530

    Create route for liking the comment

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { toggleVideoLike } from "../controllers/like.controllers.js";
+import { toggleVideoLike, toggleCommentLike } from "../controllers/like.controllers.js";
 
 const likeRouter = Router()
 likeRouter.use(verifyJWT)
 
 likeRouter.route("/:videoId/like").post(toggleVideoLike)
+likeRouter.route("/:commentId/like").post(toggleCommentLike)
 
 export default likeRouter
