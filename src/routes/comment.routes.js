6476ab6488abff055c6e7c0172a89d901079b18f
Dhairya <dhairyadave962@gmail.com>
2026-01-18 11:51:54 +0530

    Create route for updating comment

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { addComment, deleteComment } from "../controllers/comment.controllers.js"
+import { addComment, deleteComment, updateComment } from "../controllers/comment.controllers.js"
 
 const commentRouter = Router()
 commentRouter.use(verifyJWT)
 
 commentRouter.route("/:videoId/add-comment").post(addComment)
 commentRouter.route("/:commentId/delete").post(deleteComment)
+commentRouter.route("/:commentId/update").post(updateComment)
 
 export default commentRouter
