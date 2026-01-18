17cf856b67d0c564e82502db3a3d739a359dc072
Dhairya <dhairyadave962@gmail.com>
2026-01-18 12:20:19 +0530

    Create route for all comments on video

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
-import { addComment, deleteComment, updateComment } from "../controllers/comment.controllers.js"
+import { addComment, deleteComment, updateComment, getVideoComments } from "../controllers/comment.controllers.js"
 
 const commentRouter = Router()
 commentRouter.use(verifyJWT)
@@ -8,5 +8,6 @@ commentRouter.use(verifyJWT)
 commentRouter.route("/:videoId/add-comment").post(addComment)
 commentRouter.route("/:commentId/delete").post(deleteComment)
 commentRouter.route("/:commentId/update").post(updateComment)
+commentRouter.route("/videoId/get-comments").post(getVideoComments)
 
 export default commentRouter
