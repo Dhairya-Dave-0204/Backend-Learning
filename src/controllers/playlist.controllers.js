d5aaa69536a7645d143059217e515ecbf4291aaa
Dhairya <dhairyadave962@gmail.com>
2026-02-10 18:05:40 +0530

    Create controller for create playlist

---
+++ src/controllers/playlist.controllers.js
@@ -0,0 +1,35 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name || !description || !name.trim() || !description.trim()) {
+    throw new ApiError(400, "Provide details for name and description");
+  }
+
+  const playlist = await Playlist.create({
+    name: name,
+    description: description,
+    owner: req.user._id,
+  });
+
+  if (!playlist) {
+    throw new ApiError(500, "Error creating the playlist");
+  }
+
+  const populatedPlaylist = await playlist.populate("owner", "username avatar");
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, populatedPlaylist, "Playlist created successfully")
+    );
+});
+
+export {
+    createPlaylist
+};
