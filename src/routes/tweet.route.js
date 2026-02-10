fa6ff559587accb405d217891a7289c3798898a5
Dhairya <dhairyadave962@gmail.com>
2026-02-10 13:11:02 +0530

    Create route for updating tweet

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTweet, getUserTweets } from "../controllers/tweet.controllers.js"
+import { createTweet, getUserTweets, updateTweet } from "../controllers/tweet.controllers.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const tweetRouter = Router()
@@ -8,5 +8,6 @@ tweetRouter.use(verifyJWT)
 
 tweetRouter.route("/create").post(createTweet)
 tweetRouter.route("/user-tweets").get(getUserTweets)
+tweetRouter.route("/:tweetId/update").patch(updateTweet)
 
 export default tweetRouter
