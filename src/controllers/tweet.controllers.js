4737881789747f9da665152efd1c2703c5c40cbe
Dhairya <dhairyadave962@gmail.com>
2026-02-10 12:59:40 +0530

    Use $ symbol for fields in mongo

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -65,7 +65,7 @@ const getUserTweets = asyncHandler(async (req, res) => {
     },
     {
       $addFields: {
-        owner: { $first: "owner" },
+        owner: { $first: "$owner" },
       },
     },
   ]);
