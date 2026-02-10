f1ae9d1e9cb671589a7da5cbb54859f053c52996
Dhairya <dhairyadave962@gmail.com>
2026-02-10 12:32:57 +0530

    Create controller to create and get all user tweets

---
+++ src/controllers/tweet.controllers.js
@@ -0,0 +1,79 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import { User } from "../models/user.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { use } from "react";
+
+const createTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+
+  if (!content || !content.trim()) {
+    throw new ApiError(400, "Provide content for tweet");
+  }
+
+  const tweet = await Tweet.create({
+    content: content,
+    owner: req.user._id,
+  });
+
+  if (!tweet) {
+    throw new ApiError(500, "Failed to create tweet");
+  }
+
+  const populatedTweet = await tweet.populate("owner", "username avatar");
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, populatedTweet, "Tweet created successfully"));
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid user object ID");
+  }
+
+  const userTweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: { $first: "owner" },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userTweets, "Tweets fetched successfully"));
+});
+
+export { createTweet, getUserTweets };
