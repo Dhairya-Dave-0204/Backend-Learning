168b99ea47e74891d9ce70d9fa5a2d01cfacb425
Dhairya <dhairyadave962@gmail.com>
2026-02-10 12:37:32 +0530

    Create routing for tweet controller

---
+++ src/routes/tweet.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createTweet, getUserTweets } from "../controllers/tweet.controllers.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+
+const tweetRouter = Router()
+
+tweetRouter.use(verifyJWT)
+
+tweetRouter.route("/create").post(createTweet)
+tweetRouter.route("/user-tweets").get(getUserTweets)
