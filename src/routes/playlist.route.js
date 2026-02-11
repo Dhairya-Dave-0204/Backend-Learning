9386ad4e24cdb5c227937db933edb08270fe4458
Dhairya <dhairyadave962@gmail.com>
2026-02-11 10:21:04 +0530

    Create route for add video to playlist

--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPlaylist, getUserPlaylist, getPlaylistById } from "../controllers/playlist.controllers.js"
+import { createPlaylist, getUserPlaylist, getPlaylistById, addVideoToPlaylist } from "../controllers/playlist.controllers.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const playlistRouter = Router()
@@ -9,5 +9,6 @@ playlistRouter.use(verifyJWT)
 playlistRouter.route("/create").post(createPlaylist)
 playlistRouter.route("/playlist").get(getUserPlaylist)
 playlistRouter.route("/:playlistId/playlist").get(getPlaylistById)
+playlistRouter.route("/:playlistId/video/:videoId").post(addVideoToPlaylist)
 
 export default playlistRouter
