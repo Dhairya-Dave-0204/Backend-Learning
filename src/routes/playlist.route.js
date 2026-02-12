d80548a1663762e8730687714a62c078147712b6
Dhairya <dhairyadave962@gmail.com>
2026-02-12 11:25:40 +0530

    Create route for delete video from playlist

--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
-import { createPlaylist, getUserPlaylist, getPlaylistById, addVideoToPlaylist } from "../controllers/playlist.controllers.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {
+  createPlaylist,
+  getUserPlaylist,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+} from "../controllers/playlist.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const playlistRouter = Router()
+const playlistRouter = Router();
 
-playlistRouter.use(verifyJWT)
+playlistRouter.use(verifyJWT);
 
-playlistRouter.route("/create").post(createPlaylist)
-playlistRouter.route("/playlist").get(getUserPlaylist)
-playlistRouter.route("/:playlistId/playlist").get(getPlaylistById)
-playlistRouter.route("/:playlistId/video-add/:videoId").post(addVideoToPlaylist)
+playlistRouter.route("/create").post(createPlaylist);
+playlistRouter.route("/playlist").get(getUserPlaylist);
+playlistRouter.route("/:playlistId/playlist").get(getPlaylistById);
+playlistRouter
+  .route("/:playlistId/video-add/:videoId")
+  .post(addVideoToPlaylist);
+playlistRouter
+  .route("/:playlistId/video-del/:videoId")
+  .delete(removeVideoFromPlaylist);
 
-export default playlistRouter
+export default playlistRouter;
